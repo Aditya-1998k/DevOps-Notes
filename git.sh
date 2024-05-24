8177c62d70e2474fe0cb5c40f7a824e14186d95d
Aditya Gupta <102688268+Aditya-1998k@users.noreply.github.com>
2024-05-24 10:35:36 +0530

    Create git.sh

---
+++ git.sh
@@ -0,0 +1,9 @@
+GIT and GITLAB
+=============
+1. Introduction to version control
+2. Type of version control system and benefits
+3. Basic git workflow
+4. Git setup and configurables
+5. Git commands and usecases
+
+
