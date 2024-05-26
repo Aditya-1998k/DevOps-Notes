4d2c8525f1ef4516d312ae0403287d3b4ddb14a9
Aditya Gupta <102688268+Aditya-1998k@users.noreply.github.com>
2024-05-26 15:49:47 +0530

    Update linux command made easy.sh

--- linux command made easy.sh
+++ linux command made easy.sh
@@ -1,1 +1,56 @@
-#
+============================================
+           Linux commands Made Easy
+============================================
+
+Basic Commands
+--------------
+>> date
+return dates and time
+
+>> cal
+returns calander of months
+
+>> cal -y
+return whole year calender
+
+>> man cal
+you can see documents of all linux command with man <command_name>
+
+>> history
+Show executed command on terminal (sometime we forget some command
+after use)
+
+>> clear
+clear the terminal
+
+================================================================
+                  FILE SYSTEM
+================================================================
+
+/(root directory)|
+                 |boot -- containing boot system programs
+                 |user -- installed software and libraries  -- bin, lib, share, include
+                 |etc -- config files
+                 |home -- user data who logged in to the system |
+                 |dev                                           |
+                 
+                 
+                 |proc
+          
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
